ac93e1e3040ca595d8ccc4920c549e50ad3fb885
ooexiaoo <varunpaherwar@proton.me>
2026-03-20 20:32:46 +0530

    updated address

--- script.js
+++ script.js
@@ -34,5 +34,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   const map = document.getElementById('map');
-  map.innerHTML = '<iframe title="map" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3692.4392019643124!2d73.17834087529141!3d22.261346779714568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDE1JzQwLjkiTiA3M8KwMTAnNTEuMyJF!5e0!3m2!1sen!2sin!4v1742972747019!5m2!1sen!2sin"></iframe>';
+  map.innerHTML = '<iframe title="map" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14763.94363499214!2d73.16081216745654!3d22.316372674798455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc95966221d97%3A0xd33f75544929ce84!2sYogi%20Krupa%20Apartment!5e0!3m2!1sen!2sin!4v1774018883286!5m2!1sen!2sin"></iframe>';
 });
